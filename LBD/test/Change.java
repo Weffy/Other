ceb8e2a4f6c5353460acffdff4a20cb3f1e95b8a
Krirk Pongsema <k.pongsema@gmail.com>
2014-11-07 13:23:01 -0500

    Create Change.java

---
+++ LBD/test/Change.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Change
+{
+	public static void main(String[] args)
+	{
+		int randomVar = 1;		
+		String str1 = "bleh";
+		if (randomVar == 1)
+		{
+			str1 = "stuff";
+
+		}
+		System.out.println(str1);
+	}
+}
