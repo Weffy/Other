5e47e2b60220267cdd8daeebc8ef11d681cd295d
Krirk Pongsema <k.pongsema@gmail.com>
2014-11-07 13:18:10 -0500

    Create BoringWindow.java

---
+++ LBD/GUIs/BoringWindow.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class BoringWindow extends JFrame
+{
+	public static void main(String[] args)
+	{
+		JFrame f = new BoringWindow();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(800, 800);
+		f.setVisible(true);
+	}
+}
