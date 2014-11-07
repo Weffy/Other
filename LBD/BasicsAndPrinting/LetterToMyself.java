e664f4ac0175e245aee6621ba0c0331f4a5eb076
Krirk Pongsema <k.pongsema@gmail.com>
2014-11-07 13:14:11 -0500

    Create LetterToMyself.java

---
+++ LBD/BasicsAndPrinting/LetterToMyself.java
@@ -0,0 +1,18 @@
+public class LetterToMyself
+{
+	public static void main(String[] args)
+	{
+		System.out.println("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+");
+		System.out.println("|                                                          ### |");		
+		System.out.println("|                                                          ### |");		
+		System.out.println("|                                                          ### |");		
+		System.out.println("|                                                              |");		
+		System.out.println("|                                                              |");		
+		System.out.println("|                                                              |");		
+		System.out.println("|                                           Krirk Pongsema     |");		
+		System.out.println("|                                           1990 Gleason Ave   |");		
+		System.out.println("|                                           Bronx, N.Y. 10472  |");		
+		System.out.println("|                                                              |");		
+		System.out.println("+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+");
+	}
+}
